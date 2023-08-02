c436d93c236f27ec50534305b2f6f39741e3bd1c
LHackSRT <bambabar02@gmail.com>
2023-08-02 10:05:47 +0000

    test api

--- src/api/Axio.js
+++ src/api/Axio.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://54.159.196.225:443/api/v1",
+    baseURL: "http://172.31.89.205:4444/api/v1",
     headers: {
         'Content-Type' : 'application/json',
         'Accept' : 'application/json',
